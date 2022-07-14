c7d3609d92ef3f793dc31e43f21b57ae0139f0d1
mkceliks <66789068+mkceliks@users.noreply.github.com>
2022-07-15 01:25:40 +0300

    comment lines added

--- guess/guess.go
+++ guess/guess.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-func generateNum() [4]int {
+func generateNum() [4]int { // generating the random number array with size of 4.
 	var numArr [4]int
 	rand.Seed(time.Now().UnixNano())
 	for i := 0; i < 4; i++ {
@@ -16,17 +16,17 @@ func generateNum() [4]int {
 	return numArr
 }
 
-func checkGuess(computer []int, user []int) (int, int) {
-	greenCounter := 0
-	yellowCounter := 0
+func checkGuess(computer []int, user []int) (int, int) { // this is a func of handles with counters.
+	greenCounter := 0  // green counter means, one of the number of user entered is on correct place.
+	yellowCounter := 0 // yellow counter means, one of the number of user entered is in the array but on wrong place.
 	for i := 0; i < len(computer); i++ {
 		if computer[i] == user[i] {
-			greenCounter++
+			greenCounter++ // controling for green counter.
 		}
 		a := make([]int, 0)
 		a = append(a, computer[:i]...)
 		c := append(a, computer[i+1:]...)
-		for j := 0; j < len(c); j++ {
+		for j := 0; j < len(c); j++ { // controling for other indexes.
 			if user[i] == c[j] {
 				yellowCounter++
 			}
@@ -36,29 +36,28 @@ func checkGuess(computer []int, user []int) (int, int) {
 }
 
 func main() {
-	compArr := generateNum()
+	compArr := generateNum() // calling generateNum() func.
 	fmt.Println(compArr)
 	var userArr [4]int
-	prevGuesses := make([]int, 0)
-	prevGreens := make([]int, 0)
-	prevYellows := make([]int, 0)
-	for !reflect.DeepEqual(compArr, userArr) {
-
+	prevGuesses := make([]int, 0)              // this is an array for handling previous guesses.
+	prevGreens := make([]int, 0)               // this is an array for handling previous greenCounters.
+	prevYellows := make([]int, 0)              // this is an array for handling previous yellowCounters.
+	for !reflect.DeepEqual(compArr, userArr) { // equality control for slices.
 		for i := 0; i < 4; i++ {
 			fmt.Println("ENTER A NUMBER")
-			fmt.Scanln(&userArr[i])
+			fmt.Scanln(&userArr[i]) // taking numbers from user.
 		}
-		prevGuesses = append(prevGuesses, userArr[:]...)
+		prevGuesses = append(prevGuesses, userArr[:]...) // appending the guess that user made.
 
-		greenCounter, yellowCounter := checkGuess(compArr[:], userArr[:])
-		prevGreens = append(prevGreens, greenCounter)
-		prevYellows = append(prevYellows, yellowCounter)
-		for i, counter := 0, 0; i < len(prevGuesses); i, counter = i+4, counter+1 {
+		greenCounter, yellowCounter := checkGuess(compArr[:], userArr[:])           //handling the return values from checkGuess() func.
+		prevGreens = append(prevGreens, greenCounter)                               // appending the greenCounter.
+		prevYellows = append(prevYellows, yellowCounter)                            // appending the yellowCounter.
+		for i, counter := 0, 0; i < len(prevGuesses); i, counter = i+4, counter+1 { //printing the previous guesses and counters.
 			fmt.Printf("Your %d. guess is : %v ---->>> Green Counter is = %d  Yellow Counter is = %d\n", counter+1, prevGuesses[i:i+4], prevGreens[counter], prevYellows[counter])
 		}
 
 		if greenCounter == 4 {
-			fmt.Printf("You guessed correctly in %d tries! Well Done!\n", len(prevGuesses)/4)
+			fmt.Printf("You guessed correctly in %d tries! Well Done!\n", len(prevGuesses)/4) // finish print.
 			break
 		}
 	}
